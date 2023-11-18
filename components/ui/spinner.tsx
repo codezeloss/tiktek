f28c02efef00b08a9404e3271cddd148b2dd3c26
codezeloss <elhoussaineaitX@gmail.com>
2023-11-18 21:38:38 +0100

    fixing spinner color bug

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,7 +1,7 @@
 function Spinner() {
   return (
     <div className="absolute top-0 right-0 h-screen w-full flex items-center justify-center z-50 bg-white dark:bg-black">
-      <div className="w-20 h-20 rounded-full animate-spin border-8 border-solid border-black border-t-transparent dark:border-t-black" />
+      <div className="w-20 h-20 rounded-full animate-spin border-8 border-solid border-black dark:border-white border-t-transparent dark:border-t-black" />
     </div>
   );
 }
